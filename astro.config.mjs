8e1f06e24df5787f6a07df51c1efb1a0aa1077a5
roblesdotdev <httpsnip@gmail.com>
2024-12-06 21:31:03 -0300

    setup format rules with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	integrations: [tailwind({ applyBaseStyles: false })],
-});
+  server: {
+    port: 3000,
+  },
+  integrations: [tailwind({ applyBaseStyles: false })],
+})
