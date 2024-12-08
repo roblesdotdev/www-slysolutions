632170ab77f8380850bf9596581901704b375c2f
roblesdotdev <httpsnip@gmail.com>
2024-12-08 15:36:32 -0300

    custom theme tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
       fontFamily: {
         sans: ['DM Sans Variable', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        black: 'hsl(var(--black))',
+        canvas: {
+          primary: 'hsl(var(--canvas-primary))',
+          secondary: 'hsl(var(--canvas-secondary))',
+        },
+        fg: {
+          DEFAULT: 'hsl(var(--fg-default))',
+          muted: 'hsl(var(--fg-muted))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent-default))',
+          variant: 'hsl(var(--accent-variant))',
+        },
+      },
     },
   },
   plugins: [],
